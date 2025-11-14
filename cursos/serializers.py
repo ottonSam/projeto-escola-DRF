7603cb467bdf86773a32f3ebf5f4a5e03f5255b5
Samuel Otton <ottonsam@gmail.com>
2025-11-14 17:26:11 -0300

    feat: implementar app cursos

---
+++ cursos/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+
+from .models import Curso, Avaliacao
+
+class AvaliacaoSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        extra_kwargs = {
+            'email': {'write_only': True}
+        }
+        model = Avaliacao
+        fields = [
+            'id',
+            'curso',
+            'nome',
+            'email',
+            'comentario',
+            'avaliacao',
+            'criacao',
+            'ativo'
+        ]
+
+
+class CursoSerializer(serializers.ModelSerializer):
+    avaliacoes = AvaliacaoSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Curso
+        fields = [
+            'id',
+            'titulo',
+            'url',
+            'criacao',
+            'ativo',
+            'avaliacoes'
+        ]
